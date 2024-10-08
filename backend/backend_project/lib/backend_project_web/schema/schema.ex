344c82045b0735c2fc0950024287155ef95f55a0
Gerardo Franco Delgado <gfd.francodelgado@gmail.com>
2024-10-08 16:02:05 -0600

    :sparkles: feat: add health check query.

---
+++ backend/backend_project/lib/backend_project_web/schema/schema.ex
@@ -0,0 +1,59 @@
+defmodule BackendProjectWeb.Schema do
+  use Absinthe.Schema
+
+  query do
+    @desc """
+    Health check query to verify the service is operational.
+    This query provides information about the current status of the service, including its operational status, version, and current timestamp.
+
+    ## Example Query
+    ```graphql
+    query HealthCheck {
+      healthCheck {
+        status
+        version
+        timestamp
+      }
+    }
+    ```
+
+    ## Example Response
+    ```json
+    {
+      "data": {
+        "healthCheck": {
+          "status": "operational",
+          "version": "0.1.0",
+          "timestamp": "2023-04-14T12:34:56.789Z"
+        }
+      }
+    }
+    ```
+    """
+    field :health_check, :health_status do
+      resolve(&health_check_resolver/3)
+    end
+  end
+
+  @desc """
+  Represents the health status of the backend service.
+  """
+  object :health_status do
+    @desc "The current operational status of the service."
+    field :status, non_null(:string)
+
+    @desc "The current version of the backend service."
+    field :version, non_null(:string)
+
+    @desc "The current UTC timestamp when the health check was performed (ISO 8601 format)."
+    field :timestamp, non_null(:string)
+  end
+
+  def health_check_resolver(_, _, _) do
+    {:ok, %{
+      status: "operational",
+      version: BackendProject.MixProject.project()[:version],
+      timestamp: DateTime.utc_now() |> DateTime.to_iso8601()
+    }}
+  end
+end
