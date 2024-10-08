7ae083211d225833b8ba6ea0b3c51de7bd38892c
Gerardo Franco Delgado <gfd.francodelgado@gmail.com>
2024-10-08 16:01:18 -0600

    :sparkles: feat: setup graphql endpoint and graphiql in dev.

--- backend/backend_project/lib/backend_project_web/router.ex
+++ backend/backend_project/lib/backend_project_web/router.ex
@@ -1,14 +1,20 @@
 defmodule BackendProjectWeb.Router do
   use BackendProjectWeb, :router
 
-  import BackendProjectWeb.UserAuth
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/api", BackendProjectWeb do
+  scope "/api" do
     pipe_through :api
-  end
 
+    forward "/graphql", Absinthe.Plug,
+      schema: BackendProjectWeb.Schema
+
+    if Mix.env() == :dev do
+      forward "/graphiql", Absinthe.Plug.GraphiQL,
+        schema: BackendProjectWeb.Schema,
+        interface: :advanced
+    end
+  end
 end
