0a88fb21fe99545d677c1ff6f5dd9c621b6684f8
Gerardo Franco Delgado <gfd.francodelgado@gmail.com>
2024-10-08 12:37:44 -0600

    :sparkles: feat: create phoenix project.

---
+++ backend/backend_project/config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :backend_project, BackendProject.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "backend_project_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :backend_project, BackendProjectWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "lCogegCBpkVxdcfZfzR7kWP7wSgY1/ddECZyHznFMNInWhp/BTF27RK9fHaqNGNL",
+  server: false
+
+# In test we don't send emails
+config :backend_project, BackendProject.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ backend/backend_project/lib/backend_project/mailer.ex
@@ -0,0 +1,3 @@
+defmodule BackendProject.Mailer do
+  use Swoosh.Mailer, otp_app: :backend_project
+end
