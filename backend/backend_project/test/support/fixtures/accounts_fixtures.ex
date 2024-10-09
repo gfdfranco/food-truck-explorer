3e8df8384f16a4a8972bcd3364800065e4372f78
Gerardo Franco Delgado <gfd.francodelgado@gmail.com>
2024-10-08 18:43:44 -0600

    :sparkles: feat: setup files for graphql application.

--- backend/backend_project/test/support/fixtures/accounts_fixtures.ex
+++ backend/backend_project/test/support/fixtures/accounts_fixtures.ex
@@ -5,7 +5,10 @@ defmodule BackendProject.AccountsFixtures do
   """
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
-  def valid_user_password, do: "hello world!"
+  def valid_user_password, do: "Hello_world_1"
+  def valid_user_email, do: "email@unique.com"
+  def invalid_user_email, do: "user"
+  def invalid_user_password, do: "sp"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
@@ -27,5 +30,14 @@ defmodule BackendProject.AccountsFixtures do
     {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
     [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
     token
+  end
+
+  def extract_token(email) do
+    email.text_body
+    |> String.split("\n")
+    |> Enum.find(&String.contains?(&1, "some_url"))
+    |> String.split("/")
+    |> List.last()
+    |> String.trim()
   end
 end
